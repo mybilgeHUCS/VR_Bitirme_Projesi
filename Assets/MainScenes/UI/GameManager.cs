848389690e7224ba07925e908efb153bed4dfdaf
Hüsrev <Mhozbalik@gmail.com>
2024-05-17 01:23:51 +0300

    .

--- Assets/MainScenes/UI/GameManager.cs
+++ Assets/MainScenes/UI/GameManager.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadLunaparkScene(int CameraNumber)
     {
-        
-    }
+        PlayerPrefs.SetInt("CameraNumber", CameraNumber);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("RollerCoasterScene");
     }
 }
